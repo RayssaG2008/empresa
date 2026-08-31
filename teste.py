4375638c6021376e65a44677d43f0a226460cb81
RayssaG2008 <Rayssa.Gabriela@academico.ifpb.edu.br>
2026-08-31 08:14:19 -0300

    Refactor layout and styles in Streamlit app
    Refactor Streamlit app layout and styles for better image handling and text color consistency.

--- teste.py
+++ teste.py
@@ -1,44 +1,44 @@
-Claro! Aqui está o código completo em uma única parte, já com as imagens menores:
-
 import streamlit as st
 
-# Fundo branco + texto preto
 st.markdown("""
-    <style>
-    .stApp {
-        background-color: white;
-        color: black;
-    }
+<style>
+.stApp {
+    background-color: white;
+}
 
-    /* Botões */
-    .stLinkButton a {
-        background-color: #262730;
-        color: white !important;
-        text-decoration: none;
-    }
+/* Textos */
+h1, h2, h3, p {
+    color: black !important;
+}
 
-    .stLinkButton a:hover {
-        background-color: #ff4b4b;
-        color: white !important;
-    }
+/* Tamanho das imagens */
+img {
+    max-height: 100px !important;
+    width: auto !important;
+    object-fit: contain;
+}
 
-    h1, h2, h3, p, div {
-        color: black;
-    }
-    </style>
+/* Botões */
+.stLinkButton a {
+    background-color: #262730;
+    color: white !important;
+    text-decoration: none;
+}
+
+.stLinkButton a:hover {
+    background-color: #ff4b4b;
+    color: white !important;
+}
+</style>
 """, unsafe_allow_html=True)
 
-# Título
 st.title("Empresas Parceiras")
 
-# Criando 3 colunas
 col1, col2, col3 = st.columns(3)
 
-# Apple
 with col1:
     st.image(
-        "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
-        width=100
+        "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
     )
     st.title("Apple")
     st.link_button("Acessar", "https://www.apple.com/br/")
@@ -46,11 +46,9 @@ with col1:
         "Empresa responsável por uma das maiores produções de eletrônicos do mundo."
     )
 
-# Netflix
 with col2:
     st.image(
-        "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
-        width=150
+        "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
     )
     st.title("Netflix")
     st.link_button("Acessar", "https://www.netflix.com/br/")
@@ -58,15 +56,12 @@ with col2:
         "Plataforma de filmes, séries e streaming online."
     )
 
-# SpaceX
 with col3:
     st.image(
-        "https://upload.wikimedia.org/wikipedia/commons/d/de/SpaceX-Logo.svg",
-        width=150
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/SpaceX-Logo.svg"
     )
     st.title("SpaceX")
     st.link_button("Acessar", "https://www.spacex.com/")
     st.write(
         "Empresa espacial que desenvolve foguetes e missões espaciais."
     )
-
